5a3ad226ce2ff70de64ab7520aa5aafcab9bacf4
Iago Silva <iagosilvalima3@gmail.com>
2024-01-05 21:51:59 -0300

    att

--- interface.py
+++ interface.py
@@ -5,6 +5,10 @@ def exibir_data_inicio(event):
     selected_date = data_inicio.get_date()
     label_data_inicio.config(text=f"data selecionada: {selected_date}")
 
+def exibir_data_final(event):
+    selected_date = data_final.get_date()
+    label_data_termino.config(text=f"Data de término: {selected_date}")
+
 janela = Tk()
 janela.title("Automatização CISCO")
 
@@ -20,8 +24,10 @@ data_inicio.bind("<<DateEntrySelected>>", exibir_data_inicio)
 label_data_termino = Label(janela, text="Digite a data de termino:")
 label_data_termino.grid(column=3, row=0,pady=10, padx=10)
 
+
 data_final = DateEntry(janela, width=12, background='darkblue', foreground='white', borderwidth=2, year=4)
 data_final.grid(column=4, row=0,pady=10, padx=10)
+data_final.bind("<<DateEntrySelected>>", exibir_data_final)
 
 
 janela.mainloop()
