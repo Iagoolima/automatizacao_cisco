9ade7db1334afbefd4f4c19e2511c59b556da2e8
Iago Silva <iagosilvalima3@gmail.com>
2024-01-05 21:47:32 -0300

    primeiro commmit

---
+++ interface.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkcalendar import DateEntry
+
+def exibir_data_inicio(event):
+    selected_date = data_inicio.get_date()
+    label_data_inicio.config(text=f"data selecionada: {selected_date}")
+
+janela = Tk()
+janela.title("Automatização CISCO")
+
+label_data_inicio = Label(janela, text="Digite a data de inicio:")
+label_data_inicio.grid(column=0, row=0, pady=10, padx=10)
+
+data_inicio = DateEntry(janela, width=12, background='darkblue', foreground='white', borderwidth=2, year=4)
+data_inicio.grid(column=1, row=0,pady=10, padx=10)
+data_inicio.bind("<<DateEntrySelected>>", exibir_data_inicio)
+
+
+
+label_data_termino = Label(janela, text="Digite a data de termino:")
+label_data_termino.grid(column=3, row=0,pady=10, padx=10)
+
+data_final = DateEntry(janela, width=12, background='darkblue', foreground='white', borderwidth=2, year=4)
+data_final.grid(column=4, row=0,pady=10, padx=10)
+
+
+janela.mainloop()
